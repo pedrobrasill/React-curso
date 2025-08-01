b7067162a7f50e1aca6adb3d18a14d9ec7d5a91f
Pedro Eduardo <pedroedbrasil@gmail.com>
2025-07-31 21:00:17 -0300

    Task Submit

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,44 @@ function App() {
     title: "Praticar exercícios físicos",
     description: "Fazer uma sessão de treino funcional de 45 minutos.",
     isCompleted: false
-  }])
+  }]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+
+    //PRECISO ATUALIZAR ESSA TAREFA
+    if (task.id === taskId) {
+      return {...task, isCompleted: !task.isCompleted}
+    }
+
+    //NÃO PRECISO ATUALIZAR ESSA TAREFA
+    return task;
+    });
+    setTasks(newTasks);
+  };
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task => task.id != taskId));
+    setTasks(newTasks);
+      
+  }
+
+  function onAddTaskSubmit (title, description) {
+    const newTask = {
+      id: tasks.length +1,
+      title: title,
+      description: description,
+      isCpmpleted: false,
+    };
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-        <AddTask/>
-        <Tasks tasks={tasks}/>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
       
     </div>
